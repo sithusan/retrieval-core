b0d2f92c4ecf048fef65b0b2871124ca0212eb43
Si Thu San <sithusanss@gmail.com>
2026-01-01 15:33:43 +0700

    feat: linear search

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
+import os
 
 
 def main() -> None:
@@ -14,10 +16,35 @@ def main() -> None:
 
     match args.command:
         case "search":
-            print(f"Searching for: {args.query}")
+            search(args.query)
             pass
         case _:
             parser.print_help()
+
+
+def search(query: str) -> None:
+    print(f"Searching for: {query}")
+
+    limit = 5
+    movies = loadMovies()
+    foundmovies = []
+
+    for movie in movies:
+        if query in movie["title"]:
+            foundmovies.append(movie)
+            print(f"{len(foundmovies)}. {movie["title"]}")
+
+        if len(foundmovies) == limit:
+            break
+
+
+def loadMovies() -> dict:
+    abs_path = os.path.abspath("./data/movies.json")
+    movies_path = os.path.normpath(abs_path)
+
+    file = open(movies_path, "r")
+
+    return json.load(file)["movies"]
 
 
 if __name__ == "__main__":
